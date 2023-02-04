6132ab145fa2c234f243234695cc96457d14b246
Mo3bdlaa <3bdlaa.20@gmail.com>
2023-02-05 00:55:05 +0200

    fix: use Axios in filterImageFromURL

--- Project 2 - Image Filter Service/Project/src/util/util.ts
+++ Project 2 - Image Filter Service/Project/src/util/util.ts
@@ -1,6 +1,6 @@
 import fs from "fs";
-import Jimp = require("jimp");
-
+import Jimp from 'Jimp';
+import axios from 'axios';
 // filterImageFromURL
 // helper function to download, filter, and save the filtered image locally
 // returns the absolute path to the local image
@@ -11,12 +11,17 @@ import Jimp = require("jimp");
 export async function filterImageFromURL(inputURL: string): Promise<string> {
   return new Promise(async (resolve, reject) => {
     try {
-      const photo = await Jimp.read(inputURL);
-      const outpath =
-        "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
+
+      //const photo = await Jimp.read(inputURL);
+      // use axios to avoid Buffer <null> for large-size images
+      const photo = await axios({ method: 'get', url: inputURL, responseType: 'arraybuffer' })
+      .then(function ({data: imageBuffer}) {
+       return Jimp.read(imageBuffer)
+      })
+      const outpath = "/tmp/filtered." + Math.floor(Math.random() * 2000) + ".jpg";
       await photo
-        .resize(256, 256) // resize
-        .quality(60) // set JPEG quality
+        .resize(Jimp.AUTO,200) // resize  // Jimp.Auto to maintain aspect ratio
+        .quality(50) // set JPEG quality
         .greyscale() // set greyscale
         .write(__dirname + outpath, (img) => {
           resolve(__dirname + outpath);
